1a0c20b0a963a72004aff288f9c04ab6be51ca88
fatima_miramontes <fatima_3141240258@utd.edu.mx>
2025-07-24 11:27:26 -0600

    Delete PROGRAMACION_ESTRUCTURADA directory

--- PROGRAMACION_ESTRUCTURADA/P1/2_modulos_paquetes/utilizar_paquetes.py
+++
@@ -1,8 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludar("Daniel Contreras Ruano"))
-
-modulos.borrarPantalla()
-nom,tel=modulos.solicitarDatos2()
-print(f"\n\t.::Agenda Telefonica::.\n\t\tNombre: {nom}\n\t\tTelefono: {tel}")
-modulos.espereTecla()
